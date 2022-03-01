5a3f958fe2e4d1176ae2bb4c8f170a29f0adcd4f
nar2p1nk <farrisisqandar@gmail.com>
2022-03-01 23:47:54 +0800

    initialized App.vue and created new component todoList.vue

---
+++ src/components/todoList.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <h1>Todos</h1>
+        <ul>
+            <li v-for='todo of todos' :key='todo.id'>
+                {{todo.name}}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    data(){
+        return{
+            todos:[]
+        };
+    },
+    async created(){
+        try{
+            const res = await axios.get('http://localhost:8000/todos')
+
+            this.todos = res.data
+        }catch(e){
+            console.error(e);
+        }
+    }
+};
+</script>
+
+<style scoped>
+h1{
+    text-decoration: underline;
+}
+
+li{
+    color:white;
+}
+
+</style>
